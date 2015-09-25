31e11eeb13c59441e47613f8dca5cb7858908119
sambaeck <sam.baeck@student.kdg.be>
2015-09-25 12:08:41 +0200

    glucose

--- src/devices/GlucoseAdapter.java
+++ src/devices/GlucoseAdapter.java
@@ -1,17 +1,26 @@
 package devices;
 
+import vendorY.drv.GlcDrv;
+
 /**
  * Created by OEM on 25/09/2015.
  */
 public class GlucoseAdapter implements Device {
-    private int glucoseLevel;
+    private double glucoseLevel;
+    private GlcDrv glucoseDriver;
 
-    public GlucoseAdapter(int glucoseLevel) {
-        this.glucoseLevel = glucoseLevel;
+
+    public GlucoseAdapter() {
+        this.glucoseDriver = new GlcDrv();
     }
 
     @Override
     public void read() {
+        glucoseDriver.init();
+        this.glucoseLevel = glucoseDriver.get_glcXYZ_value();
+    }
 
+    public void setGlucoseLevel(double glucoseLevel) {
+        this.glucoseLevel = glucoseLevel;
     }
 }
