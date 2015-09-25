a639ba0b0d08918218af967645f6742522ae4781
sambaeck <sam.baeck@student.kdg.be>
2015-09-25 14:59:37 +0200

    filestructuur

---
+++ src/be/kdg/se3/health/engine/Parameter.java
@@ -0,0 +1,34 @@
+package be.kdg.se3.health.engine;
+
+import be.kdg.se3.health.devices.Device;
+
+/**
+ * Created by OEM on 25/09/2015.
+ */
+public class Parameter {
+    private final Device device;
+    private final int minimumValue;
+    private final int maximumValue;
+    private final int offset;
+    private final double readValue;
+
+    public Parameter(Device device, int minimumValue, int maximumValue, int offset, double readValue) {
+        this.device = device;
+        this.minimumValue = minimumValue;
+        this.maximumValue = maximumValue;
+        this.offset = offset;
+        this.readValue = readValue;
+    }
+
+    public int getMinimumValue() {
+        return minimumValue;
+    }
+
+    public int getMaximumValue() {
+        return maximumValue;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+}
