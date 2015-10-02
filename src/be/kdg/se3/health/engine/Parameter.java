ad180590b7ab6e41f59824dd464cad2a4069f5fb
Sven <sven.publie@student.kdg.be>
2015-10-02 14:21:09 +0200

    adapters and correcte manier van uitlezen

--- src/be/kdg/se3/health/engine/Parameter.java
+++ src/be/kdg/se3/health/engine/Parameter.java
@@ -6,29 +6,13 @@ import be.kdg.se3.health.devices.Device;
  * Created by OEM on 25/09/2015.
  */
 public class Parameter {
-    private final Device device;
-    private final int minimumValue;
-    private final int maximumValue;
-    private final int offset;
+    private final String id;
     private final double readValue;
 
-    public Parameter(Device device, int minimumValue, int maximumValue, int offset, double readValue) {
-        this.device = device;
-        this.minimumValue = minimumValue;
-        this.maximumValue = maximumValue;
-        this.offset = offset;
+    public Parameter(double readValue, String id) {
+        this.id = id;
         this.readValue = readValue;
     }
 
-    public int getMinimumValue() {
-        return minimumValue;
-    }
 
-    public int getMaximumValue() {
-        return maximumValue;
-    }
-
-    public int getOffset() {
-        return offset;
-    }
 }
